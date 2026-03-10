cfb410765d34e04c564b37c61ce453318ab74aa7
T A M O M <tamom.dev.git@outlook.com>
2026-03-09 23:02:39 -0400

    Dependency Injection

---
+++ MendezEmpanadas.Data/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿
+
+using MendezEmpanadas.Data.Context;
+using MendezEmpanadas.Data.Interfaces;
+using MendezEmpanadas.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MendezEmpanadas.Data
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+           services.AddDbContext<EmpanadasContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            return services;
+        }
+    }
+}
--- MendezEmpanadas.Data/obj/Debug/net10.0/MendezEmpanadas.Data.AssemblyInfo.cs
+++ MendezEmpanadas.Data/obj/Debug/net10.0/MendezEmpanadas.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MendezEmpanadas.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4afe37c84cdc03385388861d7ec322c5d89f8146")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fdf62c3827a2aaa3eccd9c29057bd104edfbb80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MendezEmpanadas.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MendezEmpanadas.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
