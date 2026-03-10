d5c61a9ff4f5db2b1e84c02b01d59a0d3b31332f
T A M O M <tamom.dev.git@outlook.com>
2026-03-09 22:12:50 -0400

    Dto change to records

---
+++ MendezEmpanadas.Data/Entities/Dtos/CreateProductRequest.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MendezEmpanadas.Data.Entities.Dtos
+{
+    public record CreateProductRequest(
+        string Name,
+        decimal Price,
+        string Description,
+        string StockQuantity
+    );
+    
+}
---
+++ MendezEmpanadas.Data/Entities/Dtos/ProductResponse.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace MendezEmpanadas.Data.Entities.Dtos
+{
+    public record ProductResponse(
+
+            Guid Id,
+            string Name,
+            decimal Price,
+            string Description,
+            string StockQuantity,
+            DateTime CreatedAt
+    );
+}
